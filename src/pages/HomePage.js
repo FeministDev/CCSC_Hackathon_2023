7369281eafd11d968de4a035c9fa2a0c0a8a86f4
Lydia Rentel <119633040+FeministDev@users.noreply.github.com>
2023-04-15 13:41:53 -0500

    This is a message. I coded.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,7 @@ import pageHeader from "../components/pageHeader";
 
 function HomePage(){
     return(
-        <div>
-            <pageHeader/>
-            <div>
-            Hello! This is the default page!
-            </div>
-        </div>
+       
     );
 }
 
